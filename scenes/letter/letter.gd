7521f182707bb60b6926911c34849f9973e103c0
Marc Sevilleja <marc.sevilleja@gmail.com>
2024-11-18 03:37:50 -0800

    Basic functionality of word game done

--- scenes/letter/letter.gd
+++ scenes/letter/letter.gd
@@ -3,6 +3,14 @@ extends CenterContainer
 
 @onready var letter: Label = $Letter
 @onready var letter_cover: Sprite2D = $LetterCover
+@onready var selector: Sprite2D = $Selector
+@onready var timer: Timer = $Timer
+
+var is_hidden: bool = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("lockpick") and is_hidden and has_focus():
+		begin_lockpick(1)
 
 
 func get_letter() -> Label:
@@ -10,4 +18,37 @@ func get_letter() -> Label:
 
 
 func hide_letter() -> void:
+	is_hidden = true
 	letter_cover.show()
+
+
+func show_letter() -> void:
+	is_hidden = false
+	selector.hide()
+	letter_cover.hide()
+	# Play unlock sound
+
+
+func _on_focus_entered() -> void:
+	# If letter is covered, allow to be lockpicked
+	if letter_cover.visible:
+		selector.show()
+
+
+func _on_focus_exited() -> void:
+	end_lockpick()
+	selector.hide()
+
+
+func begin_lockpick(duration: float) -> void:
+	timer.start(duration)
+	# play unlocking sound
+
+
+func end_lockpick() -> void:
+	timer.stop()
+
+
+func _on_timer_timeout() -> void:
+	show_letter()
+	# end unlocking sound
--- scenes/word_game/word_game.gd
+++ scenes/word_game/word_game.gd
@@ -8,18 +8,38 @@ var letter_node = preload("res://scenes/letter/letter.tscn")
 var full_word: String
 var letter_index: int
 var index_array: Array[int] = []
-var label_array: Array[Node] = []
+var letter_containers: Array[Node] = []
 var letter_array: Array[String] = []
+
 var is_game_started: bool = false
+var tries: int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and !is_game_started:
+		start_game(2)
+	
+	if Input.is_action_just_pressed("sneak"):
+		end_game()
+	
+	if Input.is_action_just_pressed("switch_focus") and is_game_started:
+		if !line_edit.has_focus():
+			print("line_edit focus:%s" % line_edit.has_focus())
+			line_edit.grab_focus()
+			print("switching focus to line_edit")
+		else:
+			letter_containers[0].grab_focus()
+			print("switching focus to letter")
+
+
 func start_game(difficulty: int) -> void:
 	is_game_started = true
-	line_edit.grab_focus()
+	setup_line_edit(difficulty)
 	
 	full_word = WordManager.get_random_word(difficulty)
 	print(full_word)
@@ -32,9 +52,9 @@ func start_game(difficulty: int) -> void:
 		full_word_container.add_child(letter_instance)
 		
 		letter_instance.get_letter().text = letter.capitalize()
-		label_array.append(letter_instance)
+		letter_containers.append(letter_instance)
 
-	randomize_letter(2, label_array.size() - 1)
+	randomize_letter(difficulty, letter_containers.size() - 1)
 
 
 func randomize_letter(difficulty: int, size: int) -> void:
@@ -42,15 +62,32 @@ func randomize_letter(difficulty: int, size: int) -> void:
 		index_array.append(n)
 	
 	# Get a random index, then remove that index from index_array to prevent dupes
-	for n in range(0, difficulty, 1):
+	for n in range(0, difficulty + 1, 1):
 		letter_index = index_array.pick_random()
 		index_array.remove_at(letter_index)
-		label_array[letter_index].hide_letter()
+		letter_containers[letter_index].hide_letter()
 	
 	index_array.clear()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and !is_game_started:
-		start_game(1)
+func setup_line_edit(diff: int) -> void:
+	line_edit.grab_focus()
+	line_edit.max_length = diff + 3
+	
+	
+func end_game() -> void:
+	is_game_started = false
+	line_edit.hide()
+	line_edit.show()
+	index_array.clear()
+	self.hide()
+	# queue_free()
+
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	if new_text == full_word:
+		print("Hooray! Correct word!")
+		end_game()
+	else:
+		print("Boo! Wrong word!")
+		line_edit.clear()
