aabe17ab264c665b25a65357788c03b9060e5c3f
Marc Sevilleja <marc.sevilleja@gmail.com>
2024-11-24 04:02:46 -0800

    Updated to use guard_mode instead of _alert flag

--- scenes/guard/guard.gd
+++ scenes/guard/guard.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 enum EnemyState { IDLE, RUN, ATTACK }
-enum PatrolState { PATROL, ALERT }
+enum GuardMode { PATROL, ALERT }
 
 
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
@@ -18,8 +18,8 @@ const DETECT_MULT: float = 10
 
 var _player: Node2D
 var _state: EnemyState = EnemyState.IDLE
-var _patrol: PatrolState = PatrolState.PATROL
-var _alerted: bool = false
+var _mode: GuardMode = GuardMode.PATROL
+# var _alerted: bool = false
 var _dec_detection: bool = false
 var _player_sighted: bool = false
 var _player_is_sneaking: bool = false
@@ -56,7 +56,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_nav_finished():
-	if _alerted:
+	if _mode == GuardMode.ALERT:
 		pass
 	else:
 		pass
@@ -72,24 +72,23 @@ func patrol(delta) -> void:
 
 
 func update_target() -> void: 
-	if _alerted:
+	if _mode == GuardMode.ALERT:
 		if _player_pos != []:
 			_player_pos.sort_custom(_sort_by_distance_to_guard)
 			nav_agent.target_position = _player_pos[0].global_position
-	else:
+	elif _mode == GuardMode.PATROL:
 		print("here at index %s" % _wp_index)
-		print(_waypoints[_wp_index])
 		nav_agent.target_position = _waypoints[_wp_index]
 
 
 func move_to_target(delta) -> void:
 	var next_path_pos = nav_agent.get_next_path_position()
 	var direction = global_position.direction_to(next_path_pos)
-	var new_velocity
-	if _alerted:
-		new_velocity = direction * alert_speed * SPEED_MULT * delta
-	else:
-		new_velocity = direction * patrol_speed * SPEED_MULT * delta
+	var new_velocity = direction * alert_speed * SPEED_MULT * delta
+	#if _mode == GuardMode.ALERT:
+		#new_velocity = direction * alert_speed * SPEED_MULT * delta
+	#elif _mode == GuardMode.PATROL:
+		#new_velocity = direction * patrol_speed * SPEED_MULT * delta
 
 	if nav_agent.avoidance_enabled:
 		nav_agent.velocity = new_velocity
@@ -110,10 +109,10 @@ func calculate_states() -> void:
 		set_enemy_states(EnemyState.RUN)
 		if velocity.x < 0:
 			sprite.flip_h = true
-			vision_cone.rotation_degrees = lerp(vision_cone.rotation_degrees, 180.0, 0.4)
+			vision_cone.rotation_degrees = lerp(vision_cone.rotation_degrees, 180.0, 0.9)
 		elif velocity.x > 0:
 			sprite.flip_h = false
-			vision_cone.rotation_degrees = lerp(vision_cone.rotation_degrees, 0.0, 0.4)
+			vision_cone.rotation_degrees = lerp(vision_cone.rotation_degrees, 0.0, 0.9)
 
 
 func set_enemy_states(new_state: EnemyState) -> void:
@@ -123,11 +122,20 @@ func set_enemy_states(new_state: EnemyState) -> void:
 	_state = new_state
 
 
-func set_patrol_states(new_state: PatrolState) -> void:
-	if _patrol == new_state:
+func set_guard_mode(new_mode: GuardMode) -> void:
+	if _mode == new_mode:
 		return
+	_mode = new_mode
 	
-	_patrol = new_state
+	match _mode:
+		GuardMode.PATROL:
+			_player_pos = []
+			follow_delay.stop()
+			update_target()
+		GuardMode.ALERT:
+			_player_pos = _player.get_nav_points()
+			follow_delay.start()
+			update_target()
 
 
 func _on_vision_cone_body_entered(body: Node2D) -> void:
@@ -161,13 +169,13 @@ func _detect_player(delta) -> void:
 	if _player_sighted:
 		if !detection_bar._get_is_alert():
 			detection_bar.detection += _sneak_detection_value * delta * DETECT_MULT
-		if detection_bar._get_is_alert():
+		if detection_bar._get_is_alert() and _mode != GuardMode.ALERT:
 			detection_bar.detection += 100 # Fill detection to max if already alerted
-			_set_alerted(true)
+			set_guard_mode(GuardMode.ALERT)
 	elif !_player_sighted and _dec_detection:
 		detection_bar.detection -= detection_dec * delta * DETECT_MULT
-		if !detection_bar._get_is_alert():
-			_set_alerted(false)
+		if !detection_bar._get_is_alert() and _mode != GuardMode.PATROL:
+			set_guard_mode(GuardMode.PATROL)
 	
 	#if _player_sighted:
 		#if _detection_level < MAX_DETECTION:
@@ -184,19 +192,19 @@ func _detect_player(delta) -> void:
 			#_detection_level = 0
 
 
-func _set_alerted(flag: bool) -> void:
-	if _alerted == flag:
-		return
-	
-	_alerted = flag
-	if _alerted and _player != null:
-		_player_pos = _player.get_nav_points()
-		follow_delay.start()
-		update_target()
-	if !_alerted:
-		_player_pos = []
-		follow_delay.stop()
-		update_target()
+#func _set_alerted(flag: bool) -> void:
+	#if _alerted == flag:
+		#return
+	#
+	#_alerted = flag
+	#if _alerted and _player != null:
+		#_player_pos = _player.get_nav_points()
+		#follow_delay.start()
+		#update_target()
+	#if !_alerted:
+		#_player_pos = []
+		#follow_delay.stop()
+		#update_target()
 	
 	# if player leaves vision cone, keep following until a set time passes
 	# if player not found after set time, reset to patrol path
@@ -212,8 +220,9 @@ func _on_detection_delay_timeout() -> void:
 
 
 func _on_navigation_agent_2d_target_reached() -> void:
-	if !_alerted:
-		if _wp_index < _waypoints.size():
+	# If patrolling and reached waypoint, update nav_target to next waypoint
+	if _mode == GuardMode.PATROL:
+		if _wp_index < _waypoints.size() - 1:
 			_wp_index += 1
 		else:
 			_wp_index = 0
